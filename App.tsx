2affe52ce4b017926f3407eef24459b1b9dc2b36
kumankoishi.py <7iggif@naver.com>
2023-08-16 00:08:23 +0900

    chore(eslint): add eslint and prettier for expo

--- App.tsx
+++ App.tsx
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>This is kuman514's React Native app!</Text>
+      <Text>Open up App.tsx to start working on!</Text>
       <StatusBar style="auto" />
     </View>
   );
