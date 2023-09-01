1f9a609b1b4401a3cc487c34b76311e257ed04cd
kumankoishi.py <7iggif@naver.com>
2023-09-01 18:55:24 +0900

    docs(convert-value): erase wrong description

--- utils/index.ts
+++ utils/index.ts
@@ -2,9 +2,7 @@ import { Animated } from 'react-native';
 
 /**
  * @description
- * I don't understand why Animated.Value from React Native does not provide its value,
- * while Animated.ValueXY does by its members x and y.
- * Anyway, this function converts Animated.Value to a number.
+ * Converts Animated.Value to a number.
  * @param value Animated.Value
  * @returns A stringified and then parsed number of parameter `value`.
  */
